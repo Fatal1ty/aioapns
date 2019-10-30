3082cdad10cae4fa97ea1437c745e43d53aa9c22
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2019-10-30 14:14:51 +0300

    Bump to 1.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aioapns',
-    version='1.8',
+    version='1.9',
     description='An efficient APNs Client Library for Python/asyncio',
     long_description=open('README.rst').read(),
     platforms="all",
