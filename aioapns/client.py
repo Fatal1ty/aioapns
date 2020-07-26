cc1cf85e713e5f88560c1e0132d5d74da95df99c
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2020-07-26 15:55:30 +0300

    Add missed no_cert_validation argument

--- aioapns/client.py
+++ aioapns/client.py
@@ -6,16 +6,19 @@ from aioapns.logging import logger
 
 
 class APNs:
-    def __init__(self,
-                 client_cert: Optional[str] = None,
-                 key: Optional[str] = None,
-                 key_id: Optional[str] = None,
-                 team_id: Optional[str] = None,
-                 topic: Optional[str] = None,
-                 max_connections: int = 10,
-                 max_connection_attempts: Optional[int] = None,
-                 loop: Optional[asyncio.AbstractEventLoop] = None,
-                 use_sandbox: bool = False):
+    def __init__(
+        self,
+        client_cert: Optional[str] = None,
+        key: Optional[str] = None,
+        key_id: Optional[str] = None,
+        team_id: Optional[str] = None,
+        topic: Optional[str] = None,
+        max_connections: int = 10,
+        max_connection_attempts: Optional[int] = None,
+        loop: Optional[asyncio.AbstractEventLoop] = None,
+        use_sandbox: bool = False,
+        no_cert_validation: bool = False,
+    ):
 
         if client_cert is not None and key is not None:
             raise ValueError('cannot specify both client_cert and key')
@@ -26,7 +29,8 @@ class APNs:
                 max_connections=max_connections,
                 max_connection_attempts=max_connection_attempts,
                 loop=loop,
-                use_sandbox=use_sandbox
+                use_sandbox=use_sandbox,
+                no_cert_validation=no_cert_validation
             )
         elif all((key, key_id, team_id, topic)):
             self.pool = APNsKeyConnectionPool(
